47994242caddbf717bbc3e109b8b5da65ac651a0
atulyadav745 <106299466+atulyadav745@users.noreply.github.com>
2022-11-01 23:29:58 +0530

    [UPDATED] Card component

--- components/cardcomponent/SliderOfCards.js
+++ components/cardcomponent/SliderOfCards.js
@@ -1,14 +1,15 @@
 import React from "react";
 import HeaderSt from "./HeaderStyles.module.css";
 import HeaderCard from "./HeaderCard";
+import Image from "next/image";
 
 function SliderOfCards() {
   return (
     <div className={HeaderSt.CardsGroup}>
-      <HeaderCard cardImage={"./ecell1.png"} cardTitle={"Type 1"} />
-      <HeaderCard cardImage={"./ecell2.png"} cardTitle={"Type 2"} />
-      <HeaderCard cardImage={"./ecell3.png"} cardTitle={"Type 3"} />
-      <HeaderCard cardImage={"./ecell4.png"} cardTitle={"Type 4"} />
+      <HeaderCard cardImage={"/header/ecell1.png"} cardTitle={"Type 1"} />
+      <HeaderCard cardImage={"/header/ecell2.jpeg"} cardTitle={"Type 2"} />
+      <HeaderCard cardImage={"/header/ecell3.jpeg"} cardTitle={"Type 3"} />
+      <HeaderCard cardImage={"/header/ecell4.jpeg"} cardTitle={"Type 4"} />
     </div>
   );
 }
--- components/helpdesk/helpdesk.js
+++ components/helpdesk/helpdesk.js
@@ -1,12 +1,9 @@
 import React, {useState} from 'react';
-// import gsap from 'gsap'
 import styles from './helpdesk.module.scss';
 import Image from "next/image";
 
-
 function Helpdesk() {
   const [selected, setSelected] = useState(null)
-
   const toggle = (i) =>{
    if (selected == i) {
      return setSelected(null)
@@ -15,43 +12,37 @@ function Helpdesk() {
    setSelected(i)
   }
   return (
-    <section className={styles.about_us}>
+    <section className={styles.container}>
 
         <div className={styles.row}>
-            <div className={styles.about_col}>
+            <div className={styles.col}>
               <p className={styles.sub_heading}>CREATE UNLIMITED LAYOUTS</p>
                 <h1>From our help desk</h1>
                 <p className={styles.sub_para}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe, perspiciatis porro deleniti nisi, mollitia, magnam
                </p>
                <div className={styles.faq}>
                 {data.map((item, i) =>(
-                  <div className='item'>
-                     <div key={i} className={styles.question} onClick = {() =>toggle(i)} >
-                     <h6 key={i}>{item.question}</h6>
+                  <div className='item' key={i} >
+                     <div className={styles.question} onClick = {() =>toggle(i)} >
+                     <h6 >{item.question}</h6>
                      <svg className={styles.hr} width="10" height="5" viewBox='0 0 42 25'> <path d = "M3 3L21 21L39 3" stroke='white' strokeWidth='7' strokeLinecap='round'/></svg>
                      </div>
                    <div className={selected == i? styles.answer_show : styles.answer}>
                     
-                <p key={i}>{item.answer}</p>
+                <p >{item.answer}</p>
              
                </div>
-
-
                      </div>
-                     
-
                 ))}
                  </div>
             </div>
-            <div className={styles.about_col} >
+            <div className={styles.col} >
             <Image src={"https://crust.winsomethemes.com/original-dark-elementor/wp-content/uploads/sites/70/2021/11/young-people.jpg"}    width="455"
         height="770" alt="image" />
             </div>
         </div>
     </section>
 
-   
-   
   )
 }
 const data = [
