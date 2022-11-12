70da9e9e0c939ebe3b07d0e02653f4d8572ff6aa
ChetankrVyas <chetankrvyas@gmail.com>
2022-11-12 14:39:39 +0530

    footer

--- components/footer.js
+++ components/footer.js
@@ -1,5 +1,5 @@
-import React from "react";
 import { Row, Col } from "react-bootstrap";
+import Image from "next/image";
 import {
   FaInstagram,
   FaFacebook,
@@ -7,9 +7,11 @@ import {
   FaTwitter,
   FaYoutube,
   FaDiscord,
+  FaEnvelope,
 } from "react-icons/fa";
 import { AiTwotoneMail } from "react-icons/ai";
 const Footer = () => {
+
   return (
     <div>
       <footer
@@ -22,74 +24,76 @@ const Footer = () => {
       >
         <div className="container p-4 pb-0">
           <section className="">
-            <div className="row">
-              <div className="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+            < Row >
+              < Col >
                 <h6 className="text-uppercase mb-4 font-weight-bold">
-                  Company name
+                  Contact Us
                 </h6>
                 <p>
-                  Here you can use rows and columns to organize your footer
-                  content. Lorem ipsum dolor sit amet, consectetur adipisicing
-                  elit.
-                </p>
-              </div>
-              <hr className="w-100 clearfix d-md-none" />
-              <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
-                <h6 className="text-uppercase mb-4 font-weight-bold">
-                  Products
-                </h6>
-                <p>
-                  <a className="text-white">MDBootstrap</a>
-                </p>
-                <p>
-                  <a className="text-white">MDWordPress</a>
-                </p>
-                <p>
-                  <a className="text-white">BrandFlow</a>
-                </p>
-                <p>
-                  <a className="text-white">Bootstrap Angular</a>
-                </p>
-              </div>
+                  Indian Institute of Technology<br />
+                  (Banaras Hindu University)<br />
+                  Varanasi, India<br />
+                  PIN: 221005<br />
 
+                  <a href="ecell@iitbhu.ac.in"><FaEnvelope></FaEnvelope> Mail us @ ecell@iitbhu.ac.in</a><br />
+                  +91 9587887413
+                </p>
+              </Col>
               <hr className="w-100 clearfix d-md-none" />
-              <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
-                <h6 className="text-uppercase mb-4 font-weight-bold">
-                  {" "}
-                  Useful links
-                </h6>
-                <p>
-                  <a className="text-white">Your Account</a>
-                </p>
-                <p>
-                  <a className="text-white">Become an Affiliate</a>
-                </p>
-                <p>
-                  <a className="text-white">Shipping Rates</a>
-                </p>
-                <p>
-                  <a className="text-white">Help</a>
-                </p>
-              </div>
+              <Col>
+                <Row>
+                  <h6 className="text-uppercase mb-4 font-weight-bold">
+                    Important Links
+                  </h6>
+                  <Col>
+                    <p>
+                      <a href="/" className="text-white ulstyle">Home</a>
+                    </p>
+                    <p>
+                      <a href="/" className="text-white ulstyle">Speaker</a>
+                    </p>
+                    <p>
+                      <a href="/" className="text-white ulstyle">Event</a>
+                    </p>
+                    <p>
+                      <a href="/" className="text-white ulstyle">Workshop</a>
+                    </p>
+                  </Col>
+
+                  <hr className="w-100 clearfix d-md-none" />
+                  <Col>
+                    <p>
+                      <a href="/" className="text-white ulstyle">Sponsers</a>
+                    </p>
+                    <p>
+                      <a href="/" className="text-white ulstyle">Team</a>
+                    </p>
+                    <p>
+                      <a href="/" className="text-white ulstyle">Campus Directors</a>
+                    </p>
+                    <p>
+                      <a href="/" className="text-white ulstyle">Help</a>
+                    </p>
+                  </Col>
+                </Row>
+              </Col>
               <hr className="w-100 clearfix d-md-none" />
-              <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
-                <h6 className="text-uppercase mb-4 font-weight-bold">
-                  Contact
-                </h6>
-                <p>
-                  <i className="fas fa-home mr-3"></i> New York, NY 10012, US
-                </p>
-                <p>
-                  <i className="fas fa-envelope mr-3"></i> info@gmail.com
-                </p>
-                <p>
-                  <i className="fas fa-phone mr-3"></i> + 01 234 567 88
-                </p>
-                <p>
-                  <i className="fas fa-print mr-3"></i> + 01 234 567 89
-                </p>
-              </div>
-            </div>
+              <Col className="footer-items">
+                <iframe
+                  src="https://ecelliitbhu.substack.com/embed"
+                  width="450"
+                  height="200"
+                  className="newsletter"
+                  style={{
+                    // border: "1px solid #EEE",
+                    background: "#f8f9fa",
+                    margin: "auto",
+                  }}
+                  // frameborder="0"
+                  scrolling="no"
+                ></iframe>
+              </Col>
+            </Row>
           </section>
 
           <hr className="my-3" />
@@ -104,36 +108,36 @@ const Footer = () => {
                 </div>
               </div>
               <div className="col-md-5 col-lg-4 ml-lg-0 text-center text-md-end">
-                <a className="btn btn-floating m-1 text-white">
+                <a className="btn btn-outline-light btn-floating m-1 text-white">
                   <FaInstagram className="social-icons"></FaInstagram>
                 </a>
                 <a
-                  className="btn btn-floating m-1 text-white iconBorder"
+                  className="btn btn-outline-light btn-floating m-1 text-white"
                   role="button"
                 >
                   {" "}
                   <FaFacebook className="social-icons"></FaFacebook>
                 </a>
                 <a
-                  className="btn btn-floating m-1 text-white"
+                  className="btn btn-outline-light btn-floating m-1 text-white"
                   role="button"
                 >
                   <FaLinkedin className="social-icons"></FaLinkedin>
                 </a>
                 <a
-                  className="btn btn-floating m-1 text-white"
+                  className="btn btn-outline-light btn-floating m-1 text-white"
                   role="button"
                 >
                   <FaTwitter className="social-icons"></FaTwitter>
                 </a>
                 <a
-                  className="btn btn-floating m-1 text-white"
+                  className="btn btn-outline-light btn-floating m-1 text-white"
                   role="button"
                 >
                   <FaYoutube className="social-icons"></FaYoutube>
                 </a>
                 <a
-                  className="btn hover-overlay hover-zoom hover-shadow btn-floating m-1 text-white"
+                  className="btn btn-outline-light btn-floating m-1 text-white"
                   role="button"
                 >
                   <FaDiscord className="social-icons"></FaDiscord>
